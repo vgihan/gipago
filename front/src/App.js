8c75d66c1026a4a330fcdf598a83822019f4de2f
vgihan <jum654@naver.com>
2022-05-27 17:20:46 +0900

    feat: add App.js
    - 최상위 컴포넌트 렌더링

---
+++ front/src/App.js
@@ -0,0 +1,36 @@
+import Router from "./components/Router.js";
+import Component from "./core/Component.js";
+
+class App extends Component {
+  template() {
+    return `<header>
+      <div class="logo">
+        <a href=""><span class="ubuntu">GIPAGO</span></a>
+      </div>
+      <nav>
+        <a href="/picture" class="picture_link gowun">사진 번역</a>
+        <a href="/file" class="file_link gowun">파일 번역</a>
+        <a href="/" class="word_link gowun">문자 번역</a>
+      </nav>
+    </header>
+    <article></article>
+    <footer>
+      <a href="https://github.com/vgihan">&copy; made by vgihan</a>
+    </footer>`;
+  }
+  setEvent() {
+    const $anchors = this.$parent.querySelectorAll("header > nav");
+    const $article = this.$parent.querySelector("article");
+
+    new Router($article, { path: location.pathname });
+
+    $anchors.forEach(($anchor) => {
+      $anchor.addEventListener("click", (e) => {
+        e.preventDefault();
+        new Router($article, { path: location.pathname });
+      });
+    });
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static("front"));
 
-app.get("", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(__dirname + "/front/index.html");
 });
 
